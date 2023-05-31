a7f4a7228998c3b3893ea6aa65f6389552f85ed6
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-05-31 09:05:03 -0500

    feat: :sparkles: evance

--- estuduiclase/api.php
+++ estuduiclase/api.php
@@ -1,11 +1,11 @@
 <?php 
-    header("Content-Type:application/json");
+/*     header("Content-Type:application/json");
     echo "datos de envio por GET";
     print_r($_GET);
     echo "datos de envio por POST";
     print_r($_POST);
 
-
+ */
 
 
 ?>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+$operacion = '(2+2)';
+
+$resultado = eval("return $operacion;");
+
+echo $resultado;
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <section class="container">
+        <form action="" method="POST">
+            <input type="submit" class="pintar" value="">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+            <input type="submit" class="numero" value="7">
+        </form>
+    </section>
+    
+</body>
+</html>
